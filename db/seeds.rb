0fd12cbb53530fb51e45c330a3f067705ac761e5
olaviolacerda <olavio.lacerda@hotmail.com>
2017-06-17 08:17:17 -0300

    seed rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-setores = [
-  ["Indústria"], ["Comércio"], ["Tecnologia"]
-]
-
-setores.each do |nome|
-  Setor.create(nome: nome)
-end
+# ruby encoding: utf-8
+setores = Setor.create! [{nome: 'Indústria'}, {nome: 'Comércio'}, {nome: 'Tecnologia'}]
+pessoa = Pessoa.create! nome: 'Olavio', email: 'olavio@realnetworking'
